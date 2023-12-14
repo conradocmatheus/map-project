739c7688490d82b0beeee78b45da38304098de8d
coradoo <conrado.matheus@protonmail.com>
2023-12-14 15:09:21 -0300

    new point selected

--- src/js/graphEditor.js
+++ src/js/graphEditor.js
@@ -3,9 +3,22 @@ class GraphEditor {
 		this.canvas = canvas;
 		this.graph = graph;
 		this.ctx = this.canvas.getContext("2d");
+		this.selected = null;
+		this.#addEventListeners();
+	}
+
+	#addEventListeners() {
+		this.canvas.addEventListener("mousedown", (event) => {
+			const mouse = new Point(event.offsetX, event.offsetY);
+			this.graph.addPoint(mouse);
+			this.selected = mouse;
+		});
 	}
 
 	display() {
 		this.graph.draw(this.ctx);
+		if (this.selected) {
+			this.selected.draw(this.ctx, { outline: true });
+		}
 	}
 }
--- src/js/primitives/point.js
+++ src/js/primitives/point.js
@@ -8,11 +8,18 @@ class Point {
 		return this.x === point.x && this.y === point.y;
 	}
 
-	draw(ctx, size = 18, color = "black") {
+	draw(ctx, { size = 18, color = "black", outline = false } = {}) {
 		const radius = size / 2;
 		ctx.beginPath();
 		ctx.fillStyle = color;
 		ctx.arc(this.x, this.y, radius, 0, 2 * Math.PI);
 		ctx.fill();
+		if (outline) {
+			ctx.beginPath();
+			ctx.lineWidth = 2;
+			ctx.strokeStyle = "yellow";
+			ctx.arc(this.x, this.y, radius * 0.6, 0, 2 * Math.PI);
+			ctx.stroke();
+		}
 	}
 }
