c388e67e1d0c7b65431b9df1cdb00c45dccd12fb
coradoo <conrado.matheus@protonmail.com>
2023-12-15 12:36:28 -0300

    reset method now in viewport.js

--- src/js/viewport.js
+++ src/js/viewport.js
@@ -1,7 +1,8 @@
 class Viewport {
 	constructor(canvas) {
 		this.canvas = canvas;
-		this.context = canvas.getContext("2d");
+		this.ctx = canvas.getContext("2d");
+
 		this.zoom = 1;
 		this.center = new Point(canvas.width / 2, canvas.height / 2);
 		this.offset = scale(this.center, -1);
@@ -12,6 +13,16 @@ class Viewport {
 			active: false,
 		};
 		this.#addEventListeners();
+	}
+
+	reset() {
+		this.ctx.restore();
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		this.ctx.save();
+		this.ctx.translate(this.center.x, this.center.y);
+		this.ctx.scale(1 / this.zoom, 1 / this.zoom);
+		const offset = this.getOffset();
+		this.ctx.translate(offset.x, offset.y);
 	}
 
 	getMouse(event, subtractDragOffset = false) {
